40eab31bedc865d2649269383e75bd89d1935fde
JahziaCCC <s300s700@gmail.com>
2026-07-19 21:02:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from sentinel import get_latest_scene
+from sentinel_process import download_preview
+import datetime as dt
 
 print("==================================================")
-print("Latest Sentinel-1 Scene")
+print("Oil Spill Intelligence V2")
 print("==================================================")
 
 # الخليج العربي
@@ -9,10 +11,27 @@ bbox = [47.0, 23.0, 56.8, 30.8]
 
 scene = get_latest_scene(bbox)
 
-if scene:
-    print(scene["id"])
-    print(scene["properties"]["datetime"])
-else:
-    print("No scene found")
+if scene is None:
+    print("No Sentinel-1 scene found.")
+    exit()
+
+print("Scene:")
+print(scene["id"])
+
+scene_time = dt.datetime.fromisoformat(
+    scene["properties"]["datetime"].replace("Z", "+00:00")
+)
+
+img = download_preview(
+    bbox,
+    scene_time - dt.timedelta(minutes=10),
+    scene_time + dt.timedelta(minutes=10),
+)
+
+print("")
+print("Image downloaded successfully.")
+print("Image shape:", img.shape)
+print("Minimum:", img.min())
+print("Maximum:", img.max())
 
 print("==================================================")
