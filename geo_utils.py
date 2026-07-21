bd9a312d9fc8c80ca4b3223ab7495b5ecb607ba9
JahziaCCC <s300s700@gmail.com>
2026-07-21 11:41:55 +0300

    Update geo_utils.py

--- geo_utils.py
+++ geo_utils.py
@@ -1,145 +1,33 @@
 import math
 
 
-EARTH_RADIUS_KM = 6371.0088
+# Sentinel-1 GRD Resolution (تقريبية)
+PIXEL_SIZE_M = 10
 
 
-
-def haversine(lat1, lon1, lat2, lon2):
-
+def estimate_area_km2(area_pixels, bbox=None, image_shape=None):
     """
-    حساب المسافة بين نقطتين بالكيلومتر
-    """
+    حساب مساحة البقعة بالكيلومتر المربع
+    باستخدام دقة Sentinel-1
 
-    lat1 = math.radians(lat1)
-    lon1 = math.radians(lon1)
-
-    lat2 = math.radians(lat2)
-    lon2 = math.radians(lon2)
-
-
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-
-
-    a = (
-
-        math.sin(dlat / 2) ** 2
-
-        +
-
-        math.cos(lat1)
-        *
-        math.cos(lat2)
-        *
-        math.sin(dlon / 2) ** 2
-
-    )
-
-
-    c = 2 * math.atan2(
-
-        math.sqrt(a),
-
-        math.sqrt(1 - a)
-
-    )
-
-
-    return EARTH_RADIUS_KM * c
-
-
-
-
-def bbox_size_km(bbox):
-
-    """
-    حساب أبعاد منطقة الرصد
-    bbox:
-    [minLon,minLat,maxLon,maxLat]
+    Sentinel-1 GRD:
+    10m x 10m تقريباً لكل بكسل
     """
 
+    pixel_area_m2 = PIXEL_SIZE_M * PIXEL_SIZE_M
 
-    min_lon, min_lat, max_lon, max_lat = bbox
+    total_area_m2 = area_pixels * pixel_area_m2
 
+    total_area_km2 = total_area_m2 / 1_000_000
 
-
-    width = haversine(
-
-        min_lat,
-
-        min_lon,
-
-        min_lat,
-
-        max_lon
-
-    )
+    return round(total_area_km2, 3)
 
 
 
-    height = haversine(
-
-        min_lat,
-
-        min_lon,
-
-        max_lat,
-
-        min_lon
-
-    )
-
-
-    return width, height
-
-
-
-
-
-def estimate_area_km2(area_pixels, bbox, image_shape):
+def pixel_resolution():
 
     """
-    حساب مساحة البقعة
-
-    يستخدم حجم البكسل الناتج من صورة Sentinel
-    وليس مساحة الـ bbox كاملة فقط
+    إرجاع دقة البكسل المستخدمة
     """
 
-
-    width_km, height_km = bbox_size_km(bbox)
-
-
-
-    image_height = image_shape[0]
-
-    image_width = image_shape[1]
-
-
-
-    pixel_width = width_km / image_width
-
-    pixel_height = height_km / image_height
-
-
-
-    pixel_area = (
-
-        pixel_width *
-
-        pixel_height
-
-    )
-
-
-
-    area = (
-
-        area_pixels *
-
-        pixel_area
-
-    )
-
-
-    return round(area, 3)
+    return PIXEL_SIZE_M
